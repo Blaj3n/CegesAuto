e5b83566478dc640a3994b08e41bafb4e3a4906a
Blaj3n <benceulrich@gmail.com>
2025-02-19 23:36:02 +0100

    2.2. feladat utoljára elvitt kocsi

--- cegesauto.py
+++ cegesauto.py
@@ -15,3 +15,10 @@ i = len(autok) - 1  # összelemszám [293]
 while autok[i][5] == 1:
     i -= 1
 print(f"{autok[i][0]}. nap rendszám: {autok[i][2]}")
+
+print("2.2. feladat ")
+for egyelem in autok:
+    if egyelem[-1] == 0:
+        utolso_kihajtas = []
+        utolso_kihajtas = [egyelem[0], egyelem[2]]
+print(f"{utolso_kihajtas[0]}. nap rendszám: {utolso_kihajtas[1]}")
