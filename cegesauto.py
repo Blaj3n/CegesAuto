e3d0d1856f3c2af7fabdabd10c9a45e8116ab8b5
Blaj3n <benceulrich@gmail.com>
2025-02-19 23:31:04 +0100

    cegesauto fájl + txt fájl beolvasása

---
+++ cegesauto.py
@@ -0,0 +1,11 @@
+# print("1. feladat")
+autok = []
+# autok_dict = []
+with open("autok.txt", "r", encoding="utf-8") as file:
+    for egysor in file:
+        egyauto = egysor.strip().split(" ")
+        autok.append([int(egyauto[0]), str(egyauto[1]), str(egyauto[2]), int(egyauto[3]),
+                      int(egyauto[4]), int(egyauto[5])])
+        # autok_dict.append({"nap": int(egyauto[0]), "idő": str(egyauto[1])})
+
+print(autok)
